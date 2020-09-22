ad9873be888ee3a6d7acd95fba90a63f4d1a3014
QiaoBaHui <qiaobahui@mockuai.com>
2020-09-22 15:34:05 +0800

    fix

---
+++ MKApp/ViewController.swift
@@ -0,0 +1,17 @@
+//
+//  ViewController.swift
+//  MKApp
+//
+//  Created by BaHui Qiao on 2020/9/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+  }
+  
+}
+
